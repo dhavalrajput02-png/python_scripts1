be274ee22db7234709b7ba67285500303028b679
dhavalrajput02-png <dhavalrajput02@gmail.com>
2026-05-19 17:56:52 +0530

    modified-script

--- hls-streaminfo-parser-all-layers-segment-duration.py
+++ hls-streaminfo-parser-all-layers-segment-duration.py
@@ -7,7 +7,31 @@ import re
 # ==========================================
 MASTER_MANIFEST_URLS = [
     "https://d3ptspza8nzxps.cloudfront.net/v1/master/071c0467fcd02420cdf0d8a1ca3524b96c27a151/itv_india_daily_news/indiadailylive/index.m3u8",
-    "https://d2sqrjitisvn17.cloudfront.net/v1/master/071c0467fcd02420cdf0d8a1ca3524b96c27a151/tara_tv/taratv/index.m3u8"
+"https://d2sqrjitisvn17.cloudfront.net/v1/master/071c0467fcd02420cdf0d8a1ca3524b96c27a151/tara_tv/taratv/index.m3u8",
+"https://d88z77jazwrot.cloudfront.net/v1/master/071c0467fcd02420cdf0d8a1ca3524b96c27a151/anand_tv/anandtv/index.m3u8",
+"https://d2qjmm8pp6modv.cloudfront.net/v1/master/071c0467fcd02420cdf0d8a1ca3524b96c27a151/haryana_beats/haryanabeat/index.m3u8",
+"https://d28aw9z1v9dl52.cloudfront.net/v1/master/071c0467fcd02420cdf0d8a1ca3524b96c27a151/news_malayalam_swiftv/newsmalayalamlive/index.m3u8",
+"https://dhschn7phghlj.cloudfront.net/v1/master/071c0467fcd02420cdf0d8a1ca3524b96c27a151/punjabi_hits/punjabihits/index.m3u8",
+"https://d2c64wlzqq0ngq.cloudfront.net/v1/master/071c0467fcd02420cdf0d8a1ca3524b96c27a151/rongeen_tv/rongeentv/index.m3u8",
+"https://d27p16vtqgequl.cloudfront.net/v1/master/071c0467fcd02420cdf0d8a1ca3524b96c27a151/rplus/hls5/index.m3u8",
+"https://dd1587ggje2rn.cloudfront.net/v1/master/071c0467fcd02420cdf0d8a1ca3524b96c27a151/lakshya_tv/distrolakshya/index.m3u8",
+"https://dat3ebe9q09hb.cloudfront.net/v1/master/071c0467fcd02420cdf0d8a1ca3524b96c27a151/kalyan_tv/distrotvkalyantv/index.m3u8",
+"https://d3foatmq523jy6.cloudfront.net/v1/master/071c0467fcd02420cdf0d8a1ca3524b96c27a151/kartvya_tv/distrotvkartavyatv/index.m3u8",
+"https://d1rjy43nstmb82.cloudfront.net/v1/master/071c0467fcd02420cdf0d8a1ca3524b96c27a151/living_india/livingindia/index.m3u8",
+"https://d34ty5mrc8bxua.cloudfront.net/v1/master/071c0467fcd02420cdf0d8a1ca3524b96c27a151/tabbar_hits/tabbarhits/index.m3u8",
+"https://d3i4nfibrjst21.cloudfront.net/v1/master/071c0467fcd02420cdf0d8a1ca3524b96c27a151/news_tamil_swifttv/newstamil/index.m3u8",
+"https://d22euq38lyxy6x.cloudfront.net/v1/master/071c0467fcd02420cdf0d8a1ca3524b96c27a151/pratidin_times/pratidin/index.m3u8",
+"https://d1vlbhvyvl59gv.cloudfront.net/v1/master/071c0467fcd02420cdf0d8a1ca3524b96c27a151/munsif_tv/munsiftv/index.m3u8",
+"https://ddfiowhw29pcs.cloudfront.net/v1/master/071c0467fcd02420cdf0d8a1ca3524b96c27a151/global_punjab/distroglobalpunjab/index.m3u8",
+"https://ds37poyi3xm8n.cloudfront.net/v1/master/071c0467fcd02420cdf0d8a1ca3524b96c27a151/mahuaa_play/mahuaplayjio/index.m3u8",
+"https://d1yak0wyh733r.cloudfront.net/v1/master/071c0467fcd02420cdf0d8a1ca3524b96c27a151/mahuaa_khabar/mahuakhabarjio/index.m3u8",
+"https://d1lxslxfgt5l3u.cloudfront.net/v1/master/071c0467fcd02420cdf0d8a1ca3524b96c27a151/mahaa_news/mahaanews/index.m3u8",
+"https://d23babkhkiy0fk.cloudfront.net/v1/master/071c0467fcd02420cdf0d8a1ca3524b96c27a151/mahaa_maxx/mahaamaxx/index.m3u8",
+"https://dk6cuki64nihv.cloudfront.net/v1/master/071c0467fcd02420cdf0d8a1ca3524b96c27a151/sandesh_news/sandeshnews/index.m3u8",
+"https://d13f1k5bcoch3z.cloudfront.net/v1/master/071c0467fcd02420cdf0d8a1ca3524b96c27a151/abn_andhrajyoti/abnandhrajyothycdn/index.m3u8",
+"https://d1rzdclb147bs.cloudfront.net/v1/master/071c0467fcd02420cdf0d8a1ca3524b96c27a151/kolkata_tv/kolkatatv/index.m3u8",
+"https://d3esntc4thfeda.cloudfront.net/v1/master/071c0467fcd02420cdf0d8a1ca3524b96c27a151/sakshi_tv/sakshitv/index.m3u8",
+"https://d2uk2hcw7n1tv9.cloudfront.net/v1/master/071c0467fcd02420cdf0d8a1ca3524b96c27a151/big_tv/bigtv/index.m3u8"
 ]
 
 # ==========================================
